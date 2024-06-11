bfda8c542b3b731ee6d6ed8cb439845999cfd9f4
Tarikokc <tarik.oukaci@gmail.com>
2024-06-11 20:49:35 +0200

    Inscription faite !

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -1,23 +1,4 @@
 <?php
-
-// namespace App\Controller;
-
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\Routing\Attribute\Route;
-
-// class ProduitController extends AbstractController
-// {
-//     #[Route('/produit', name: 'app_produit')]
-//     public function index(): Response
-//     {
-//         return $this->render('produit/index.html.twig', [
-//             'controller_name' => 'ProduitController',
-//         ]);
-//     }
-// }
-
-
 // src/Controller/ProduitController.php
 namespace App\Controller;
 
